e277f8d34d4a9a22c7542891ecf7edc03c313840
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2023-10-17 17:56:53 -0300

    hotfix

--- src/main/java/animatch/app/utils/CorsConfiguration.java
+++ src/main/java/animatch/app/utils/CorsConfiguration.java
@@ -1,5 +1,4 @@
 package animatch.app.utils;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
