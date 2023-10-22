53f31fdf89e94ea23d81f9688894039faeee4cb3
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2023-10-22 02:27:21 -0300

    arrumando classes domain

--- src/main/java/animatch/app/domain/anime/Anime.java
+++ src/main/java/animatch/app/domain/anime/Anime.java
@@ -16,6 +16,8 @@ public class Anime {
     @NotNull
     @Schema(description = "Id do anime na API anilist", example = "253")
     private int idApi;
+    @Schema(description = "Titulo do anime", example = "Naruto")
+    private String nome;
     @Schema(description = "Nota média do anime provida pela API", example = "56.7")
     private double notaMedia;
     @Schema(description = "Link para imagem do anime", example = "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx113415-bbBWj4pEFseh.jpg")
@@ -24,25 +26,51 @@ public class Anime {
     public Anime() {
     }
 
-    public Anime(int id, int idApi, double notaMedia) {
+    public Anime(int id, int idApi, String nome, double notaMedia, String imagem) {
         this.id = id;
         this.idApi = idApi;
+        this.nome = nome;
         this.notaMedia = notaMedia;
+        this.imagem = imagem;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public int getIdApi() {
         return idApi;
+    }
+
+    public void setIdApi(int idApi) {
+        this.idApi = idApi;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public double getNotaMedia() {
         return notaMedia;
     }
 
+    public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+
     public String getImagem() {
         return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
     }
 }
--- src/main/java/animatch/app/dto/AnimeInfoDTO.java
+++ src/main/java/animatch/app/dto/AnimeInfoDTO.java
@@ -4,6 +4,9 @@ import animatch.app.domain.anime.Anime;
 
 public class AnimeInfoDTO {
     private Anime anime;
+    private String coverImage;
+    private String nome;
+
 
     public AnimeInfoDTO(Anime anime) {
         this.anime = anime;
